92ecf26952fec10cda4ecbcd91ade076b0415e1f
Lucia Capozzo <capozzo.lu@gmail.com>
2022-08-11 15:57:53 +0200

    minor import fix

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -2,3 +2,4 @@ export { default as BarChart } from './BarChart';
 export { default as LineChart } from './LineChart';
 export { default as DoughnutChart } from './Doughtnut';
 export { default as PieChart } from './PieChart';
+export { default as ScatterChart } from './ScatterChart';
