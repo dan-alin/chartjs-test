3206795e949262397cb94d14a063d7efa801584b
Dan Alin <alin.ioan.dan@accenture.com>
2022-09-05 18:07:38 +0200

    stye:gauge and doughnut chart

--- src/utils/generators/generators.tsx
+++ src/utils/generators/generators.tsx
@@ -91,7 +91,7 @@ const chartDataGenerator = (
           label: `Set ${_i}`,
           data: fakeData,
           borderColor: 'rgb(255, 255, 255)',
-          backgroundColor: range(labels).map(() => faker.color.rgb()),
+          backgroundColor: colors,
           hoverOffset: 4,
         };
         break;
@@ -100,7 +100,7 @@ const chartDataGenerator = (
           label: `Set ${_i}`,
           data: fakeData,
           borderColor: 'rgb(255, 255, 255)',
-          backgroundColor: range(labels).map(() => faker.color.rgb()),
+          backgroundColor: colors,
           hoverOffset: 4,
           needleValue: faker.datatype.number(gaugeNeedleRange),
         };
@@ -128,8 +128,10 @@ const chartDataGenerator = (
           label: `Set ${_i}`,
           data: fakeData,
           borderColor: datasetColor,
+          borderWidth: 1,
           backgroundColor: transparentize(datasetColor, 0.7),
           fill: false,
+          tension: 0.1,
         };
     }
 
