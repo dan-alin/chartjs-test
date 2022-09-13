16f7b9acb750bf459233a400b49b049977450e16
Dan Alin <alin.ioan.dan@accenture.com>
2022-09-13 10:58:36 +0200

    commnt button to deploy

--- src/components/charts/bar-chart.component.tsx
+++ src/components/charts/bar-chart.component.tsx
@@ -13,7 +13,7 @@ import {
 import { BarChartProps } from '@typings/charts.d';
 import { getDefaultData } from 'src/utils/configurations/chart-config';
 import useChart from 'src/hooks/use-chart.hook';
-import { Button } from '@components/button';
+// import { Button } from '@components/button';
 
 ChartJS.register(
   CategoryScale,
@@ -29,7 +29,7 @@ const BarChart: FC<BarChartProps> = ({
   customOptions = {},
   customData = getDefaultData(),
 }) => {
-  const { data, chartRef, options, generateNewData } = useChart(
+  const { data, chartRef, options } = useChart(
     'bar',
     customOptions,
     customData
@@ -46,7 +46,7 @@ const BarChart: FC<BarChartProps> = ({
           ref={chartRef}
         />
       </div>
-      <Button onClick={() => generateNewData(3, 3, 'bar')}>New Data</Button>
+      {/* <Button onClick={() => generateNewData(3, 3, 'bar')}>New Data</Button> */}
     </>
   );
 };
